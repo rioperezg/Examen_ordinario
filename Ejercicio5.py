87e3d9591559a8acdef776317dadcc8c35aab854
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-12-19 09:52:35 +0100

    15th commit

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,5 +1,17 @@
 from ast import main
+"""""
+Cree una función hollow_triangle(altura) que devuelva un triángulo hueco de la altura correcta. La altura se pasa a través de 
+la función y la función debe devolver una lista que contiene cada línea del triángulo hueco.
 
+hollow_triangle(6)
+
+ should return : ['_____#_____', '____#_#____', '___#___#___', '__#_____#__', '_#_______#_', '###########']
+
+hollow_triangle(9)
+
+ should return : ['________#________', '_______#_#_______', '______#___#______', '_____#_____#_____', '____#_______#____', 
+ '___#_________#___', '__#___________#__', '_#_____________#_', '#################']
+"""
 
 
 
