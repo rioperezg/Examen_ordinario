f233b84dfc5238c074d810b318b85641263d9455
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-12-19 09:13:57 +0100

    6th commit

--- Ejerciciio3.py
+++ Ejerciciio3.py
@@ -17,8 +17,42 @@ y nueve).
 
 Para cualquier número entero entre 0 y 999, devuelva una matriz que muestre la ruta desde ese número entero hasta un equilibrio estable:
 """
-
-
+# Debemos equilibrar el numero de letras de cada palabra = al numero en si
+def Equilibrio(n):
+    # Decclaramos los numero primero del 1 al 10
+    numeros_iniciales= [0,1,2,3,4,5,6,7,8,9]
+    cero = 0
+    uno = 1
+    dos = 2
+    tres = 3
+    cuatro = 4
+    cinco = 5
+    seis = 6
+    siete = 7
+    ocho = 8
+    nueve = 9
+    for i in range(0,10):
+        if numeros_iniciales[i] == n:
+            if numeros_iniciales[i] == 0:
+                return cero
+            elif numeros_iniciales[i] == 1:
+                return uno
+            elif numeros_iniciales[i] == 2:
+                return dos
+            elif numeros_iniciales[i] == 3:
+                return tres
+            elif numeros_iniciales[i] == 4:
+                return cuatro
+            elif numeros_iniciales[i] == 5:
+                return cinco
+            elif numeros_iniciales[i] == 6:
+                return seis
+            elif numeros_iniciales[i] == 7:
+                return siete
+            elif numeros_iniciales[i] == 8:
+                return ocho
+            elif numeros_iniciales[i] == 9:
+                return nueve
 
 
 
