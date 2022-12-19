11096e05644c8d9672561e9aa62364b4566d4829
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-12-19 11:02:06 +0100

    32th commit

--- Ejerciciio8.py
+++ Ejerciciio8.py
@@ -1,7 +1,33 @@
 from ast import main
 """""
-
+¡A nadie le gustan los lunes! Pasaste el fin de semana de fiesta y con amigos, y luego llega el lunes y tienes que levantarte 
+temprano, ponerte ropa de negocios e ir a trabajar. Entonces, ¿cuántos de estos horribles días tiene que soportar alguien? Bueno, 
+averigüémoslo.
+Cree un método para encontrar el número de lunes dado el cumpleaños de una persona y la fecha actual. Para este ejercicio simple, 
+no te preocupes por los días festivos/vacaciones, licencia por enfermedad, etc. Supongamos que una persona tiene un trabajo y va a 
+trabajar durante todo el año si está en edad de trabajar. Para simplificar las cosas, suponga que alguien comienza a trabajar cuando 
+tiene 22 años y se jubila cuando tiene 78.
+¡Así es, los lunes no cuentan cómo días malos si estás en la escuela/universidad o eres jubilado!
+En las pruebas, la fecha actual será la misma o posterior al cumpleaños de una persona y ambas fechas serán no nulas y válidas. 
+Y aunque no tener que trabajar los fines de semana es una moda bastante reciente (¡búscalo!), asume que los lunes siempre serán y 
+serán días malos en cualquier época.
+Para resolver este problema, se puede seguir los siguientes pasos:
+Calcular la edad de la persona en la fecha actual. Para ello, se puede calcular la diferencia en años entre la fecha actual y la 
+fecha de cumpleaños de la persona.
+Verificar si la persona está en edad de trabajar. Para ello, se puede comprobar si su edad está entre 22 y 78 años.
+Calcular el número de lunes que hay entre la fecha de cumpleaños y la fecha actual. Para ello, se puede calcular el número de días 
+que hay entre ambas fechas y dividirlo por 7, ya que una semana tiene 7 días.
+Si la persona está en edad de trabajar, devolver el número de lunes calculado en el paso 3. En caso contrario, devolver 0
 """
+def lunes(cumpleaños, fecha_actual):
+    lista = []
+    for i in cumpleaños:
+        if i == 0:
+            for j in fecha_actual:
+                if j == 1:
+                    cumpleaños[i] = cumpleaños[i].replace(j)
+                    lista.append(cumpleaños[i] - cumpleaños[j])
+    return lista
 
 
 
---
+++ main.py
@@ -0,0 +1,11 @@
+import Ejercicio1, Ejerciciio2, Ejerciciio3, Ejerciiio4, Ejercicio5, Ejerciiio6, Ejerciciio7, Ejerciciio8 
+
+if __name__=="__main__":
+    Ejercicio1.simplify()
+    Ejerciciio2.main()
+    Ejerciciio3.main()
+    Ejerciiio4.main()
+    Ejercicio5.main()
+    Ejerciiio6.main()
+    Ejerciciio7.main()
+    Ejerciciio8.main()
