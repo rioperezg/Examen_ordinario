80f7f267044cbb72e3c43a87bb70f31093316974
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-12-19 10:21:25 +0100

    28th commit

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -16,7 +16,7 @@ def Hollow_triangle(n):
     for i in range(1, n+1):
         print(("_"*(n-i) + "#") + ("_"*(2*i-3) + "#")*(i>1) + ("_"*(n-i)))    
     return None
-print(Hollow_triangle(6))
+print(Hollow_triangle(10))
 
 
 
