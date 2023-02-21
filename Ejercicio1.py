0e7ef0be9d7e9f71a654c742398999608284a135
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-21 18:15:48 +0100

    imbecil

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -4,11 +4,16 @@ Para este ejercicio, creará un solucionador de Nonogramas. :)
 
 Si no sabe qué son los nonogramas, puede consultar algunas instrucciones y también probar algunos nonogramas aquí.
 
-Para resolver un Nonograma, primero debemos entender cómo funcionan. Un Nonograma es un rompecabezas que consta de una matriz de celdas en blanco y negro. Las pistas se proporcionan en los bordes de la matriz en forma de números que indican cuántas celdas negras hay en cada fila o columna consecutiva.
+Para resolver un Nonograma, primero debemos entender cómo funcionan. Un Nonograma es un rompecabezas que consta de una matriz 
+de celdas en blanco y negro. Las pistas se proporcionan en los bordes de la matriz en forma de números que indican cuántas 
+celdas negras hay en cada fila o columna consecutiva.
 
-Por ejemplo, si las pistas para una fila son [2, 1], significa que hay dos grupos de celdas negras consecutivas en esa fila, el primero con dos celdas y el segundo con una sola celda. La solución para esta fila sería "BB_X_B" (donde "B" representa una celda negra y "X" representa una celda en blanco).
+Por ejemplo, si las pistas para una fila son [2, 1], significa que hay dos grupos de celdas negras consecutivas en esa fila, 
+el primero con dos celdas y el segundo con una sola celda. La solución para esta fila sería "BB_X_B" (donde "B" representa una 
+celda negra y "X" representa una celda en blanco).
 
-Para resolver un Nonograma completo, debemos usar las pistas y nuestro conocimiento de cómo funcionan los Nonogramas para ir rellenando la matriz poco a poco hasta obtener la solución final.
+Para resolver un Nonograma completo, debemos usar las pistas y nuestro conocimiento de cómo funcionan los Nonogramas para ir 
+rellenando la matriz poco a poco hasta obtener la solución final.
 
 solo tendrás que resolver Nonogramas 5x5. :)
 
@@ -29,9 +34,11 @@ class Nonogram:
 
         pass
 
-Se te darán las pistas y deberás devolver el rompecabezas resuelto. Todos los acertijos se podrán resolver, por lo que no tendrás que preocuparte por eso. Habrá exactamente una solución para cada rompecabezas.
+Se te darán las pistas y deberás devolver el rompecabezas resuelto. Todos los acertijos se podrán resolver, por lo que no 
+tendrás que preocuparte por eso. Habrá exactamente una solución para cada rompecabezas.
 
-Las pistas serán una tupla de las pistas de columna, luego las pistas de fila, que contendrán las pistas individuales. Por ejemplo, para el Nonograma:
+Las pistas serán una tupla de las pistas de columna, luego las pistas de fila, que contendrán las pistas individuales. 
+Por ejemplo, para el Nonograma:
 
     |   |   | 1 |   |   |
 
@@ -63,8 +70,12 @@ Las pistas serán una tupla de las pistas de columna, luego las pistas de fila, 
 
 Las pistas están en la parte superior e izquierda del rompecabezas, así que en este caso:
 
-Las pistas de columna son: ((1, 1), (4,), (1, 1, 1), (3,), (1,)), y las pistas de fila son: ((1,), (2,), (3,), (2, 1), (4,)). Las pistas de la columna se dan de izquierda a derecha. Si hay más de una pista para la misma columna, la pista superior se da primero. Las pistas de fila se dan de arriba a abajo. Si hay más de una pista para la misma fila, la pista más a la izquierda se da primero.
-Por lo tanto, la pista dada al __init__método sería (((1, 1), (4,), (1, 1, 1), (3,), (1,)), ((1,), (2,), (3,), (2, 1), (4,))). Se le dan las pistas de la columna primero y luego las pistas de la fila en segundo lugar.
+Las pistas de columna son: ((1, 1), (4,), (1, 1, 1), (3,), (1,)), y las pistas de fila son: ((1,), (2,), (3,), (2, 1), (4,)). 
+Las pistas de la columna se dan de izquierda a derecha. Si hay más de una pista para la misma columna, la pista superior se da 
+primero. Las pistas de fila se dan de arriba a abajo. Si hay más de una pista para la misma fila, la pista más a la izquierda se 
+da primero.
+Por lo tanto, la pista dada al __init__método sería (((1, 1), (4,), (1, 1, 1), (3,), (1,)), ((1,), (2,), (3,), (2, 1), (4,))). 
+Se le dan las pistas de la columna primero y luego las pistas de la fila en segundo lugar.
 Debe devolver una tupla de las filas como su respuesta. En este caso, el Nonograma resuelto se ve así:
 
     |   |   | 1 |   |   |
